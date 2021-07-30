f25ffc1119c249eedecf56ee327932575e4d9c6d
Frank Harkins <frankharkins@hotmail.co.uk>
2021-07-30 16:29:35 +0100

    add text linter script via vale

--- scripts/text_lint.py
+++ scripts/text_lint.py
@@ -1,10 +1,13 @@
-import nbformat, os
+import nbformat, os, subprocess
+
+TEMP_PATH = './scripts/temp'
+NOTEBOOKS_PATH = './notebooks'
 
 def extract_markdown(filepath):
     """Extracts markdown from notebook at <filepath> for linting
     with vale. Stores extracted markdown in `scripts/vale/temp/`.
     """
-    nb = nbformat.read(f'notebooks/{filepath}.ipynb', as_version=4)
+    nb = nbformat.read(f'{NOTEBOOKS_PATH}/{filepath}.ipynb', as_version=4)
     md = ""
     for cell in nb.cells:
         if cell['cell_type'] == 'markdown':
@@ -16,10 +19,10 @@ def extract_markdown(filepath):
             continue
         out += line + '\n'
 
-    newpath = f"scripts/vale/temp/{filepath[::-1].split('/',1)[1][::-1]}"
+    newpath = f"{TEMP_PATH}/{filepath[::-1].split('/',1)[1][::-1]}"
     if not os.path.exists(newpath):
         os.makedirs(newpath)
-    with open(f'scripts/vale/temp/{filepath}.md', 'w') as f:
+    with open(f'{TEMP_PATH}/{filepath}.md', 'w') as f:
         f.write(out)
 
 if __name__ == '__main__':
@@ -30,3 +33,5 @@ if __name__ == '__main__':
         if filepath.startswith('#') or filepath=='':
             continue
         extract_markdown(filepath)
+
+    vale_return_code = subprocess.run(['vale', TEMP_PATH]).returncode
